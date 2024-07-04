9ad4da0e96a550008e0595c439735df88ae994c3
m-ue-d <fabian.mild@gmx.at>
2024-07-04 16:00:34 +0200

    Started implementing project- and music-panel

--- src/components/MusicPanel.tsx
+++ src/components/MusicPanel.tsx
@@ -1,7 +1,12 @@
 import styles from './MusicPanel.module.scss';
 
 export default function MusicPanel(){
-    return <div>
+    return <div class={styles.container}>
         <h2>MY MUSIC</h2>
+        <ul class={styles.songs}>
+            {/* fetch songs from soundcloud */}
+            <li><iframe width="35%" height="166" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1783453722&color=%23111b16&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/m-ue-d" title="m-ue-d" target="_blank" style="color: #cccccc; text-decoration: none;">m-ue-d</a> · <a href="https://soundcloud.com/m-ue-d/surrogate" title="Surrogate" target="_blank" style="color: #cccccc; text-decoration: none;">Surrogate</a></div></li>
+            <li><iframe width="35%" height="166" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/1783453722&color=%23111b16&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe><div style="font-size: 10px; color: #cccccc;line-break: anywhere;word-break: normal;overflow: hidden;white-space: nowrap;text-overflow: ellipsis; font-family: Interstate,Lucida Grande,Lucida Sans Unicode,Lucida Sans,Garuda,Verdana,Tahoma,sans-serif;font-weight: 100;"><a href="https://soundcloud.com/m-ue-d" title="m-ue-d" target="_blank" style="color: #cccccc; text-decoration: none;">m-ue-d</a> · <a href="https://soundcloud.com/m-ue-d/surrogate" title="Surrogate" target="_blank" style="color: #cccccc; text-decoration: none;">Surrogate</a></div></li>
+        </ul>   
     </div>;
 }
--- src/components/ProjectPanel.tsx
+++ src/components/ProjectPanel.tsx
@@ -1,7 +1,12 @@
 import styles from './ProjectPanel.module.scss';
 
 export default function ProjectPanel(){
-    return <div>
-    <h2>MY PROJECTS</h2>
-</div>;
+    return <div class={styles.container}>
+        <h2>MY PROJECTS</h2>
+        <ul class={styles.projects}>
+            <li>Test1</li>
+            <li>Test2</li>
+            <li>Test3</li>
+        </ul>
+    </div>;
 }
