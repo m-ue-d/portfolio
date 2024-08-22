51ba4db4533f5ae6e85b902522d77dea2c455760
m-ue-d <fabian.mild@gmx.at>
2024-08-22 17:27:54 +0200

    import fix 2

--- src/components/HoverLink.tsx
+++ src/components/HoverLink.tsx
@@ -7,9 +7,13 @@ export interface HoverLinkProps {
     active: boolean;
 }
 
+export const getImageUrl = (path: string) => {
+    return new URL(`${path}`, import.meta.url).href;
+};
+
 export default function HoverLink(props: HoverLinkProps) {
 
     return <div class={styles.container} data-show={props.active}>
-        <a href={props.href}>{props.imageUrl.startsWith("/src/") && <img src={props.imageUrl} />}{props.imageUrl.startsWith("<svg") && <div class={styles.logoContainer} innerHTML={props.imageUrl} />}{props.name}</a>
+        <a href={props.href}>{props.imageUrl.startsWith("/src/") && <img src={getImageUrl(props.imageUrl)} />}{props.imageUrl.startsWith("<svg") && <div class={styles.logoContainer} innerHTML={props.imageUrl} />}{props.name}</a>
     </div>
 }
