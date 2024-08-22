162ff078b67d69517f012e180a21ba50d7906513
m-ue-d <fabian.mild@gmx.at>
2024-08-22 17:38:51 +0200

    import fix infinity

--- src/components/HoverLink.tsx
+++ src/components/HoverLink.tsx
@@ -7,13 +7,9 @@ export interface HoverLinkProps {
     active: boolean;
 }
 
-export const getImageUrl = (path: string) => {
-    return new URL(`${path}`, import.meta.url).href;
-};
-
 export default function HoverLink(props: HoverLinkProps) {
 
     return <div class={styles.container} data-show={props.active}>
-        <a href={props.href}>{props.imageUrl.startsWith("/src/") && <img src={getImageUrl(props.imageUrl)} />}{props.imageUrl.startsWith("<svg") && <div class={styles.logoContainer} innerHTML={props.imageUrl} />}{props.name}</a>
+        <a href={props.href}>{props.imageUrl.startsWith("<") && <div class={styles.logoContainer} innerHTML={props.imageUrl} />}{props.name}</a>
     </div>
 }
